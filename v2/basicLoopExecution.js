8dea79f9242fcf7e77c5efe15a780d464492f001
Asif Isthiaq <asif.isthiaq@aheeva.com>
2024-02-24 23:27:12 -0500

    Add basic loop execution with settimeout to test different scopes

---
+++ v2/basicLoopExecution.js
@@ -0,0 +1,23 @@
+// for(let i=1;i<=3;i++){
+//     setTimeout(()=>{
+//         console.log(i);
+//     })
+// }
+
+for(let i=1;i<=3;i++){
+    setTimeout(()=>{
+        console.log(++i);
+    })
+}
+
+// for(var i=1;i<=3;i++){
+//     setTimeout(()=>{
+//         console.log(i);
+//     })
+// }
+
+// for(var i=1;i<=3;i++){
+//     setTimeout(()=>{
+//         console.log(++i);
+//     })
+// }
